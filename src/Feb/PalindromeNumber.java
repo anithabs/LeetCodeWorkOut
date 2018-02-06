d0dacd8850187f19079ac7581835c673632439cc
Anitha Boppagowdanapura Shivalingaswamy <anithabs.ec@gmail.com>
2018-02-06 15:55:58 -0600

    02/06/2018

--- src/Feb/PalindromeNumber.java
+++ src/Feb/PalindromeNumber.java
@@ -2,13 +2,13 @@ package Feb;
 
 public class PalindromeNumber {
 	public static void main(String arg[]){
-		int x = 123454321;
+		int x = 0;
 		PalindromeNumber pn = new PalindromeNumber();
 		boolean result = pn.isPalindromNumber(x);
 		System.out.println(result);
 	}
 	public boolean isPalindromNumber(int x){
-		if(x < 0)return false;
+		if(x < 0 || (x != 0 && (x % 10 == 0)))return false;
 		int rev = 0;
 		while(x > rev){
 			rev = rev * 10 + x % 10;
